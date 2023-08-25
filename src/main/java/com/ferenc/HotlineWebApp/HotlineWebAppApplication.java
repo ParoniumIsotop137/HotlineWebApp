f0170e6430d4d28f4018a23a3c7243c8f0591bdb
ParoniumIsotop137 <horwathslos29@gmail.com>
2023-08-25 09:47:58 +0200

    v0.0

---
+++ src/main/java/com/ferenc/HotlineWebApp/HotlineWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.ferenc.HotlineWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotlineWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotlineWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferenc/HotlineWebApp/HotlineWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferenc.HotlineWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotlineWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
