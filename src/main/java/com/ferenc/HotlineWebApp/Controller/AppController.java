40757a0a2e11b2a3e019f314a1c10443aa7f522c
ParoniumIsotop137 <horwathslos29@gmail.com>
2023-08-28 19:33:14 +0200

    v0.3

--- src/main/java/com/ferenc/HotlineWebApp/Controller/AppController.java
+++ src/main/java/com/ferenc/HotlineWebApp/Controller/AppController.java
@@ -1,18 +1,39 @@
 package com.ferenc.HotlineWebApp.Controller;
 
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.ferenc.HotlineWebApp.entity.HotlineDataWeb;
 
 @Controller
 
 public class AppController {
 
 	
-	@RequestMapping("/hotlinehome")
+	@GetMapping("/hotlinehome")
 	public String HotlineWebApp() {
 		
 		return "mainPage";
+		
+	}
+	
+	@GetMapping("/register")
+    public String showRegPage() {
+        return "regPage";
+    }
+	
+	@PostMapping("/register")
+	public String fromRegPage(@ModelAttribute HotlineDataWeb data) {
+		data.setDate();
+		System.out.println(data.toString());
+		return "regPage";
 		
 	}
 	
--- src/main/java/com/ferenc/HotlineWebApp/entity/HotlineDataWeb.java
+++ src/main/java/com/ferenc/HotlineWebApp/entity/HotlineDataWeb.java
@@ -2,22 +2,114 @@ package com.ferenc.HotlineWebApp.entity;
 
 import java.time.LocalDate;
 
+import org.springframework.stereotype.Component;
+
 import com.ferenc.hotlineapp.mainwindow.HotlineData;
 
-public class HotlineDataWeb extends HotlineData{
-	
-	private boolean isClosed;
 
-	public HotlineDataWeb(LocalDate date, int sdNumber, String openedFrom, String technicianName, String phoneNumber,
-			String localizationNumber, int lineNumber, String closedFrom, boolean isClosed) {
-		super(date, sdNumber, openedFrom, technicianName, phoneNumber, localizationNumber, lineNumber, closedFrom);
+public class HotlineDataWeb {
+	
+	private LocalDate date;
+	private int sdNumber;
+	private String openedFrom;
+	private String technicianName;
+	private String phoneNumber;
+	private String localizationNumber;
+	private int lineNumber;
+	private String closedFrom;
+	private boolean isClosed;
+	
+	
+	public HotlineDataWeb() {
+		
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate() {
+		this.date = LocalDate.now();
+	}
+
+	public int getSdNumber() {
+		return sdNumber;
+	}
+
+	public void setSdNumber(int sdNumber) {
+		this.sdNumber = sdNumber;
+	}
+
+	public String getOpenedFrom() {
+		return openedFrom;
+	}
+
+	public void setOpenedFrom(String openedFrom) {
+		this.openedFrom = openedFrom;
+	}
+
+	public String getTechnicianName() {
+		return technicianName;
+	}
+
+	public void setTechnicianName(String technicianName) {
+		this.technicianName = technicianName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getLocalizationNumber() {
+		return localizationNumber;
+	}
+
+	public void setLocalizationNumber(String localizationNumber) {
+		this.localizationNumber = localizationNumber;
+	}
+
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	public void setLineNumber(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+
+	public String getClosedFrom() {
+		return closedFrom;
+	}
+
+	public void setClosedFrom(String closedFrom) {
+		this.closedFrom = closedFrom;
+	}
+
+	public boolean isClosed() {
+		return isClosed;
+	}
+
+	public void setClosed(boolean isClosed) {
 		this.isClosed = isClosed;
 	}
 
+	
+
 	@Override
 	public String toString() {
-		return super.toString()+", isClosed=" + ((this.isClosed)?"Lezárt":"Nyitott");
+		return "HotlineDataWeb [date=" + date + ", sdNumber=" + sdNumber + ", openedFrom=" + openedFrom
+				+ ", technicianName=" + technicianName + ", phoneNumber=" + phoneNumber + ", localizationNumber="
+				+ localizationNumber + ", lineNumber=" + lineNumber + ", closedFrom=" + closedFrom + ", isClosed="
+				+ isClosed + "]";
 	}
+	
+	
+	
+	
+
 	
 	
 	
